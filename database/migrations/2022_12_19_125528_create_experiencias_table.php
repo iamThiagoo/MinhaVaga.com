da70e8a0ff6d10d92f0b2136d1edff42fa12de26
Thiago Ferreira <thiagofer2003@outlook.com>
2022-12-19 13:13:53 -0300

    Inicio na criação das migrations

---
+++ database/migrations/2022_12_19_125528_create_experiencias_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('experiencias', function (Blueprint $table) {
+            $table->id();
+
+            $table->char('name', 200);
+            $table->date('initial_date');
+            $table->date('final_date');
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->unsignedBigInteger('empresa_id');
+            $table->foreign('empresa_id')->references('id')->on('empresa');
+
+            $table->unsignedBigInteger('cidade_id');
+            $table->foreign('cidade_id')->references('id')->on('cidade');
+
+            $table->unsignedBigInteger('estado_id');
+            $table->foreign('estado_id')->references('id')->on('estado');
+
+            $table->text('funcao')->nullable();
+            $table->boolean('status')->default(true);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('experiencias');
+    }
+};
