5a2f28b8b5ed4d0691c7a31cd99a019bcac6b5e2
Thiago Ferreira <devthiagoferreira@gmail.com>
2022-12-22 00:53:27 -0300

    Voltando ao desenvolvimento da view de criar perfil inicial

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -34,26 +34,26 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-xxl-5 col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 d-flex align-items-start flex-column">
-                        <div class="input-group flex-column">
-                            <label>Nome Completo</label>
-                            <input class="my-2" type="text" name="name">
+                    <div class="col-xxl-5 col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 d-flex align-items-start flex-column about-user">
+                        <div class="btn-add-group d-flex w-100 justify-content-between">
+                            <p>Adicionar Experiência</p>
+                            <p>+</p>
                         </div>
-                        <div class="input-group flex-column">
-                            <label>Nome Completo</label>
-                            <input class="my-2" type="text" name="name">
+                        <div class="btn-add-group d-flex w-100 justify-content-between">
+                            <p>Adicionar Formação Acadêmica</p>
+                            <p>+</p>
                         </div>
-                        <div class="input-group flex-column">
-                            <label>Nome Completo</label>
-                            <input class="my-2" type="text" name="name">
+                        <div class="btn-add-group d-flex w-100 justify-content-between">
+                            <p>Adicionar Licenças ou Certificados</p>
+                            <p>+</p>
                         </div>
-                        <div class="input-group flex-column">
-                            <label>Nome Completo</label>
-                            <input class="my-2" type="text" name="name">
+                        <div class="btn-add-group d-flex w-100 justify-content-between">
+                            <p>Adicionar Competências</p>
+                            <p>+</p>
                         </div>
-                        <div class="input-group flex-column">
-                            <label>Nome Completo</label>
-                            <input class="my-2" type="text" name="name" >
+                        <div class="btn-add-group d-flex w-100 justify-content-between">
+                            <p>Adicionar Idiomas</p>
+                            <p>+</p>
                         </div>
                         <button class="mt-4" type="submit">Salvar perfil <i class="fa-solid fa-right-to-bracket"></i> </button>
                     </div>
@@ -61,6 +61,53 @@
             <p class="text-center mt-5 support-text">Se você possui alguma dúvida/feedback nos envie através do email <a href="#">contato@minhavaga.com.br</a>, <br> vamos adorar recebê-la.</p>
         </section>
     </main>
+
+    {{-- <div class="modal-container">
+        <div class="modal-box">
+            <h2>Adicionar Experiência</h2>
+            <div>
+                <div class="input-group flex-column ">
+                    <label for="">Nome do cargo</label>
+                    <input type="text">
+                </div>
+                <div class="input-group flex-column ">
+                    <label for="">Nome da empresa</label>
+                    <input type="text">
+                </div>
+                <div class="input-group flex-column ">
+                    <label for="">Tipo da vaga</label>
+                    <input type="text">
+                </div>
+                <div class="d-flex">
+                    <div class="input-group flex-column ">
+                        <label for="">Estado</label>
+                        <input type="text">
+                    </div>
+                    <div class="input-group flex-column ">
+                        <label for="">Cidade</label>
+                        <input type="text">
+                    </div>
+                </div>
+                <div>
+                    <div class="d-flex">
+                        <div class="input-group flex-column">
+                            <label for="">Data Inicial</label>
+                            <input type="date">
+                        </div>
+                        <div class="input-group flex-column">
+                            <label for="">Data de Término</label>
+                            <input type="date">
+                        </div>
+                    </div>
+                    <input type="checkbox" name="" id="">Trabalho nesse cargo atualmente
+                </div>
+                <div class="input-group input-group-textarea flex-column">
+                    <label for="">Função/Descrição do cargo</label>
+                    <textarea></textarea>
+                </div>
+            </div>
+        </div>
+    </div> --}}
 
 @endsection
 
@@ -86,7 +133,6 @@
                 $('.camera-img-icon').removeClass('d-none');
                 $('.user-photo').css('filter', 'brightness(0.3)');
             }
-
         });
 
         function loadFile(event) {
